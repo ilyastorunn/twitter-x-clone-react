8433540ed9a22b27fed1b302effe9e69ac471ed7
ilyastorunn <ilyastorunn@outlook.com>
2023-10-16 01:31:50 +0300

    font size fixes

--- src/layouts/main/sidebar/account/more/index.jsx
+++ src/layouts/main/sidebar/account/more/index.jsx
@@ -23,14 +23,14 @@ export default function More({ close }) {
                 })}
                 >
                     <img src={account.avatar} className="w-10 h-10 rounded-full" alt="" />
-                    <div className="mx-3 flex-1 text-[15px]">
-                        <h6 className="font-bold leading-[20px]">{account.fullName}</h6>
+                    <div className="mx-3 flex-1">
+                        <h6 className="font-bold leading-[1.25rem]">{account.fullName}</h6>
                         <div className="text-[color:var(--color-base-secondary)]">
                             @{account.username}
                         </div>
                     </div>
                     {currentAccount.id === account.id && (
-                        <svg viewBox="0 0 24 24" className="mr-2 ml-3" width={18.75} height={18.75} fill="#00ba7c">
+                        <svg viewBox="0 0 24 24" className="mr-2 ml-3 h-[1.172rem]" fill="#00ba7c">
                             <path 
                                 d="M12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-.81 14.68l-4.1-3.27 1.25-1.57 2.47 1.98 3.97-5.47 1.62 1.18-5.21 7.15z"                                
                             />
@@ -39,15 +39,15 @@ export default function More({ close }) {
                 </button>
             ))}
             <div className="h-px bg-[color:var(--background-third)] my-3" />
-            <button className="py-3 px-4 text-left transition-colors hover:bg-[color:var(--background-secondary)] w-fulltext-[15px] font-bold leading-[20px]">
+            <button className="py-3 px-4 text-left transition-colors hover:bg-[color:var(--background-secondary)] w-full font-bold leading-[1.25rem]">
                 Add an existing account
             </button>
-            <button className="py-3 px-4 text-left transition-colors hover:bg-[color:var(--background-secondary)] w-fulltext-[15px] font-bold leading-[20px]">
+            <button className="py-3 px-4 text-left transition-colors hover:bg-[color:var(--background-secondary)] w-full font-bold leading-[1.25rem]">
                 Manage accounts
             </button>
-            <button className="py-3 px-4 text-left transition-colors hover:bg-[color:var(--background-secondary)] w-fulltext-[15px] font-bold leading-[20px]">
+            <button className="py-3 px-4 text-left transition-colors hover:bg-[color:var(--background-secondary)] w-full font-bold leading-[1.25rem]">
                 <div className="max-w-[228px]">
-                    Log out @ilyastorunn
+                    Log out @{currentAccount.username}
                 </div>
             </button>
         </div>
