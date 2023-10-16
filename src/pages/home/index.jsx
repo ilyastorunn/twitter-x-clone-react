d8be8b2522c33fcaff6c70b93c57c8781c45cb21
ilyastorunn <ilyastorunn@outlook.com>
2023-10-16 11:39:10 +0300

    created tab component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,27 @@
+import StickyHeader from "~/components/sticky-header";
+import Tab from "~/components/tab";
+
 export default function Home() {
     return (
-        <div>
-            home component!
-        </div>
+        <>
+            <Tab activeTab="following">
+                <StickyHeader title="Home">
+                    <Tab.Items>
+                        <Tab.Item id="for-you">
+                            For you
+                        </Tab.Item>
+                        <Tab.Item id="following">
+                            Following
+                        </Tab.Item>
+                    </Tab.Items>
+                </StickyHeader>
+                <Tab.Content id="for-you">
+                    1. Content
+                </Tab.Content>
+                <Tab.Content id="following">
+                    2. Content
+                </Tab.Content>
+            </Tab>
+        </>
     )
 }
